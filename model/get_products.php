bdc04bf318b921500da785e1c24207247d9e5749
sbr66 <bonam22@gmail.com>
2022-12-23 12:09:02 +0900

    image upload php

---
+++ model/get_products.php
@@ -0,0 +1,14 @@
+<?php
+
+    // SELECT * FROM [table name] WHERE [option]
+    // 1. DB 접속
+    // 2. sql 쿼리 실행
+    // 3. 쿼리 결과 json 배열로 응답
+    include_once $_SERVER['DOCUMENT_ROOT'].'/main_backend/connect/dbconn.php';
+
+    $sql = "SELECT * FROM spl_products ORDER BY pro_reg DESC LIMIT 6"; //DESC:역순, ASC:정순(dafalt) 정렬
+    $result = mysqli_query($conn, $sql); //  첫번째 파라미터 : 접속정보, 두번째 파라미터 ; 쿼리문
+
+    echo json_encode(array("msg"=>mysqli_fetch_array($result)));
+
+?>
--- model/product_upload.php
+++ model/product_upload.php
@@ -12,7 +12,7 @@
     $pro_name=$_POST['product_name'];
     $pro_price=$_POST['product_price'];
     $pro_desc=nl2br($_POST['product_desc']); // nl2br : 엔터로 개행시 br태그 자동 입력
-    $pro_desc=addslashes($pro_desc); // 따옴표 중복 작성시 '/' 처리를 자동으로 실행
+    // $pro_desc=addslashes($pro_desc); // 따옴표 중복 작성시 '/' 처리를 자동으로 실행
     
     // 날짜 변수
     $pro_reg=date("Y-m-d H:i:s");
@@ -29,10 +29,10 @@
     $exp = array_pop(explode('.', $image_name)); // '.'을 기준으로 뒷부분 문자열 반환
 
     // 파일 사이즈 제한 변수
-    $limit = 50000;
+    $limit = 500000;
 
     // 확장자 필터링
-    if(!in_array($exp, $allowed_exp)){ // in_array : 두번재 파라미터 배열에 첫번재 파라미터 문자열이 잇으면 true
+    if(!in_array($exp, $allowed_exp)){ // in_array : 두번재 파라미터 배열에 첫번재 파라미터 문자열이 있으면 true
         echo json_encode(array("msg" => "허용하지 않는 이미지 파일 형식입니다."));
         exit();
     }
@@ -43,12 +43,44 @@
         exit();
     }
 
+    $image_dir=$_SERVER['DOCUMENT_ROOT'].'/main_project/images/products/'; //사진이 저장 될 경로 저장
+
     // 이미지 업로드 : *파일 업로드 시 파일 자체는 지정한 디렉토리 안으로 가고, DB에는 파일 이름 문자열이 저장된다.
     if($image_name && !$image_error){
-        
+        // move_uploaded_file(파일 임시 이름, 이동할위치)
+        $upload_file=$image_dir.$image_name; // 변수에 경로 저장
+        if(!move_uploaded_file($image_tmp_name, $upload_file)){
+            echo json_encode(array("msg" => "사진이 업로드 되지 않았습니다."));
+            exit();
+        } 
     }else{
         $image_name="";
     }
+    // 접속 정보 로드
+    include_once $_SERVER['DOCUMENT_ROOT'].'/main_backend/connect/dbconn.php';
+
+    // sql 입력 명령어 입력
+    $sql = "INSERT INTO spl_products (pro_name, pro_pri, pro_desc, pro_img, pro_reg) VALUES (?,?,?,?,?)";
+
+    //stmt init 참조 :https://www.w3schools.com/Php/func_mysqli_stmt_init.asp
+    $stmt = $conn->stmt_init();
+
+    if(!$stmt->prepare($sql)){
+        http_response_code(400);
+        echo json_encode(array("msg" => "Database insert fail."));
+    }
+
+    $stmt -> bind_param("sssss", $pro_name, $pro_price, $pro_desc, $image_name, $pro_reg);
+    $stmt -> execute();
+    
+    if($stmt->affected_rows > 0){
+        http_response_code(200);
+        echo json_encode(array("msg" => "입력이 완료되었습니다"));
+    }else{
+        http_response_code(400);
+        echo json_encode(array("msg"=>"입력에 실패하셨습니다"));
+    }
+
 
     // echo json_encode(array("name" => "$pro_name", "price" => $pro_price, "desc" => $pro_desc, "reg" => $pro_reg, "img_name" => $image_name, "tmp_name" => $image_tmp_name, "type" => $image_type, "size" => $image_size, "img_error" => $image_error, "exp" =>$exp));
 
