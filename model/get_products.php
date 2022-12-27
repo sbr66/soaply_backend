9915e25e6e4da0b73ec8a7b95b64bf57f888ef00
sbr66 <bonam22@gmail.com>
2022-12-27 11:52:48 +0900

    detail page...

--- model/get_products.php
+++ model/get_products.php
@@ -9,6 +9,10 @@
     $sql = "SELECT * FROM spl_products ORDER BY pro_reg DESC LIMIT 6"; //DESC:역순, ASC:정순(dafalt) 정렬
     $result = mysqli_query($conn, $sql); //  첫번째 파라미터 : 접속정보, 두번째 파라미터 ; 쿼리문
 
-    echo json_encode(array("msg"=>mysqli_fetch_array($result)));
 
+    $json_result = array(); // 빈 배열 초기화
+    while($row = mysqli_fetch_array($result)){
+        array_push($json_result, array("pro_idx" => $row['pro_idx'], "pro_name" => $row['pro_name'], "pro_pri" => $row['pro_pri'], "pro_desc" => $row['pro_desc'], "pro_img" => $row['pro_img'], "pro_reg" => $row['pro_reg'])); // 첫번째 파라미터: 대상 배열, 두번째 파라미터는 배열 입력값
+    }
+    echo json_encode($json_result);
 ?>
